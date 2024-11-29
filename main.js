4430befa2dce12b579989191c3bcebc0716886be
fillwithjoy1 <29514446+fillwithjoy1@users.noreply.github.com>
2024-11-29 18:57:44 +1000

    Simplify code 2

--- main.js
+++ main.js
@@ -316,17 +316,11 @@ export async function download_single_video(website_url, file_name = "", folder_
     });
 
     if (!file_name || append_file_name) {
-        await page.goto(website_url, {
-            waitUntil: "domcontentloaded",
-        });
-        console.log("oh I work!")
         const title = await page.$eval('._3JyyHX', t => t.innerHTML);
         console.log(title);
         console.log("Hello!")
         console.log(file_name);
         file_name += title;
-    } else {
-        await page.goto(website_url);
     }
 
     function start_downloads(video_link, license_url, file_name) {
