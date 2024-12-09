9addaf49c9c40d411fe6a2f65bddd56dafed899f
fillwithjoy1 <29514446+fillwithjoy1@users.noreply.github.com>
2024-12-10 00:14:24 +1000

    Compile jobs.js

--- jobs.js
+++ jobs.js
@@ -1,7 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var main_1 = require("./main");
 var fs = require("node:fs");
 if (fs.existsSync("jobs.json")) {
+    fs.readFile("jobs.json", "utf8", function (err, data) {
+        if (err) {
+            console.error(err);
+        }
+        var jobs = JSON.parse(data);
+        for (var i = 0; i < jobs.jobs.length; i++) {
+            if (jobs.jobs[i].skip) {
+                continue;
+            }
+            (0, main_1.browser_mass_download)(jobs.jobs[i].link, jobs.jobs[i].folder_name, jobs.jobs[i].length);
+        }
+    });
 }
 else {
     console.log("There is no jobs.json created");
