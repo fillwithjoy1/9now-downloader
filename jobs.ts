cb764c270a6f2247eae07c74c093f289ec69e93b
fillwithjoy1 <29514446+fillwithjoy1@users.noreply.github.com>
2024-12-10 00:10:57 +1000

    Add finishing touches

--- jobs.ts
+++ jobs.ts
@@ -15,7 +15,18 @@ interface JobSchema {
 }
 
 if (fs.existsSync("jobs.json")) {
-
+    fs.readFile("jobs.json", "utf8", (err, data): void => {
+        if (err) {
+            console.error(err);
+        }
+        const jobs: JobSchema = JSON.parse(data);
+        for (let i = 0; i < jobs.jobs.length; i++) {
+            if (jobs.jobs[i].skip) {
+                continue;
+            }
+            browser_mass_download(jobs.jobs[i].link, jobs.jobs[i].folder_name, jobs.jobs[i].length);
+        }
+    });
 } else {
     console.log("There is no jobs.json created");
     console.log("Create a jobs.json file to run a download job as needed");
