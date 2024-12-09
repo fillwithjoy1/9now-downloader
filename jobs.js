31d82cc8edc0336011c9d63bd8f1ea33a09d7d1e
fillwithjoy1 <29514446+fillwithjoy1@users.noreply.github.com>
2024-12-09 23:08:10 +1000

    Update jobs.ts 2

--- jobs.js
+++ jobs.js
@@ -1,8 +1,9 @@
-import * as fs from "node:fs";
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = require("node:fs");
 if (fs.existsSync("jobs.json")) {
-
-} else {
+}
+else {
     console.log("There is no jobs.json created");
     console.log("Create a jobs.json file to run a download job as needed");
 }
