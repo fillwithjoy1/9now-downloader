0a608492dc8288e2b3f81b7f4994576fdf947f83
fillwithjoy1 <29514446+fillwithjoy1@users.noreply.github.com>
2024-12-15 15:19:28 +1000

    Test lock functions

--- main.test.js
+++ main.test.js
@@ -1,5 +1,7 @@
-import {Browser} from "./functions.js";
+import {Browser, Lock} from "./functions.js";
 import {expect, test} from "vitest";
+import * as fs from "node:fs/promises";
+import {existsSync} from "node:fs"
 
 // TODO: Add cohesive testing before reworking the files
 test("404 Page Check", async () => {
@@ -13,3 +15,22 @@ test("404 Page Check", async () => {
 
     expect(await browser.check404(page)).toBeTruthy();
 }, 30000);
+
+// Test Lock class
+test("Create a lock", async () => {
+    if (existsSync("node.lock")) await fs.unlink("node.lock");
+    await Lock.lock();
+    expect(await fs.stat("node.lock")).toBeTruthy();
+    await fs.unlink("node.lock");
+});
+
+test("Remove a lock", async () => {
+    if (!existsSync("node.lock")) await Lock.lock();
+    Lock.unlock();
+    expect(existsSync("node.lock")).toBeFalsy();
+})
+
+test("Check Lock.status()", async () => {
+    if (!existsSync("node.lock")) await Lock.lock();
+    expect(Lock.status()).toBeTruthy();
+});
