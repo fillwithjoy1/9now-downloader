99dda2c7dec7d6db185d97b1d99051ecebcfef56
fillwithjoy1 <29514446+fillwithjoy1@users.noreply.github.com.>
2025-01-02 23:51:26 +1000

    Rework main.ts 3

--- main.ts
+++ main.ts
@@ -1,19 +1,5 @@
 import * as readline from "node:readline/promises";
-import * as fs from "node:fs";
 import {Browser, browser_mass_download, python_download_video, Lock} from "./functions.js";
-
-// Credentials Manager
-let email = "";
-let password = "";
-if (fs.existsSync("password")) {
-    fs.readFile("password", "utf8", (err, data) => {
-        if (err) {
-            console.error(err);
-        }
-        email = data.split("\r\n")[0];
-        password = data.split("\r\n")[1];
-    });
-}
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -29,7 +15,7 @@ rl.question(`Download one video, or an entire playlist? (1, 2) `).then(async opt
             break;
         case 1:
             const url_1 = await rl.question(`Video URL`);
-            await rl.close();
+            rl.close();
             const browser = await Browser.create();
             await Lock.lock();
             const data_1 = await browser.downloadSingleVideo(url_1);
@@ -41,11 +27,11 @@ rl.question(`Download one video, or an entire playlist? (1, 2) `).then(async opt
             const url_2 = await rl.question(`Playlist URL `);
             const folder_2 = await rl.question(`Output folder name `);
             const length_2 = await rl.question(`Playlist length `);
-            await rl.close();
+            rl.close();
             await browser_mass_download(url_2, folder_2, length_2);
             break;
         default:
             console.log('Invalid choice. Exiting');
             process.exit(2);
-}
+    }
 });
